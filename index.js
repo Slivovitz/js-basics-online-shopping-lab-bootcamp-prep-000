b76dd264dbfe05cbb866e56a244c4e68886e5a8e
Yosef Margolin <yukele@hotmail.com>
2017-11-16 01:49:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -80,5 +80,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(!cardNumber) {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
 }

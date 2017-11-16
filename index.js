d04c12598316c08e8428003e9f5c757432e3e06a
Yosef Margolin <yukele@hotmail.com>
2017-11-16 02:00:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -80,7 +80,12 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
+  var orderTotal = total()
   if(!cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
+  } else {
+    console.log(`Your total cost is \$${orderTotal}, which will be charged to the card ${cardNumber}.`)
   }
+  cart = []
+  
 }
